ee371b7da032b3619979d2406e3eb0d254567047
LetsGoPol <polpostigo@gmail.com>
2025-02-03 15:37:25 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 function mostrarMensaje() {
     alert("¡Hola! Has hecho clic en el botón.");
 }
+
+function mostrarTienda() {
+    let tienda = document.getElementById("productos");
+    if (tienda.classList.contains("hidden")) {
+        tienda.classList.remove("hidden");
+    } else {
+        tienda.classList.add("hidden");
+    }
+}
+
+function registrarVoto(voto) {
+    let resultado = document.getElementById("resultado");
+    resultado.textContent = "Gracias por tu voto: " + voto;
+}
